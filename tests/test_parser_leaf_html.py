5682b8a3cfc3c37baf4adef17fc6fc41631ead20
Stefan Hagen <stefan@hagen.link>
2020-10-04 09:42:38 +0200

    Activated explorative test to work with pytest - unfinished business but passing

--- tests/test_parser_leaf_html.py
+++ tests/test_parser_leaf_html.py
@@ -1,15 +1,32 @@
+import pathlib
 from bs4 import BeautifulSoup
-with open("leaf_page_sample.html", "rt", encoding="utf-8") as handle:
-    html_text = handle.read()
-soup = BeautifulSoup(html_text, 'html.parser')
-lines = [t for t in html_text.split('\n') if t.startswith('<a href="')]
-parsed = []
-for line in lines:
-    a, x = line.split('">', 1)
-    f, r = x.split('</a>')
-    r = r.rstrip()
-    d, s, u = r.rsplit(' ', 2)
-    d = d.strip()
-    parsed.append((f, d, s, u))
-for p in parsed:
-    print(p)
+
+HTML_LEAF_PAGE_SAMPLE_PATH = pathlib.Path('tests', 'fixtures', 'html', 'leaf_page_sample.html')
+HTML_TEXT = ''
+
+
+def setup():
+    global HTML_TEXT
+    with open(HTML_LEAF_PAGE_SAMPLE_PATH, "rt", encoding="utf-8") as handle:
+        HTML_TEXT = handle.read()
+
+
+def teardown():
+    global HTML_TEXT
+    HTML_TEXT = ''
+
+
+def test_html_leaf_page_parse_fixture():
+    # soup = BeautifulSoup(HTML_TEXT, 'html.parser')
+    lines = [t for t in HTML_TEXT.split('\n') if t.startswith('<a href="')]
+    parsed = []
+    for line in lines:
+        a, x = line.split('">', 1)
+        f, r = x.split('</a>')
+        r = r.rstrip()
+        d, s, u = r.rsplit(' ', 2)
+        d = d.strip()
+        parsed.append((f, d, s, u))
+    for p in parsed:
+        print(p)
+    assert len(p) == 4
