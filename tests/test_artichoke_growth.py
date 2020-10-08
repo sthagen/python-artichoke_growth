a77bb33a9eb4d73d13aa25e9279c01f12409f5ec
Stefan Hagen <stefan@hagen.link>
2020-10-08 10:35:24 +0200

    Update test_artichoke_growth.py

--- tests/test_artichoke_growth.py
+++ tests/test_artichoke_growth.py
@@ -9,3 +9,9 @@ def test_by_name_ok_minimal():
     text = '0123456789abcdef'
     hash_length = len(text)
     assert ag.by_name(text, hash_length) is True
+
+
+def test_by_name_nok_minimal():
+    text = '0123456789abcdeg'
+    hash_length = len(text)
+    assert ag.by_name(text, hash_length) is False
